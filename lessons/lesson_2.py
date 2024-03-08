8bfcc3b7ce69df1589a7567afab8ad647ccc046f
Hanna Negash <hnegash.n@gmail.com>
2024-03-07 17:44:11 -0800

    Add a file with uncompleted codes

---
+++ lessons/lesson_2.py
@@ -0,0 +1,42 @@
+# If something is True or False (boolean expression)
+    
+statement_one = "Dogs are mammals."  # This is boolean expression
+
+statement_two = "My dog is named Pavel." # This is boolean expression
+
+statement_three = "Dogs make the best pets." # This is not boolean expression
+
+statement_four = "Cats are female dogs." # This is boolean expression
+
+
+                                #_______________________________________________________________
+                                
+# Relational Operators: Equals and Not Equals - (Relational operators compare two items and return either True or False)
+    # Equals: ==  |  Not equals: !=
+
+                                #_______________________________________________________________
+
+# Boolean Variables => True and False are the only bool types, and any variable that is assigned one of these values is called a boolean variable.
+
+# Boolean variables - assign True or False to a variable
+set_to_true = True
+set_to_false = False
+
+# Set a variable equal to a boolean expression - variable contains boolean values
+
+bool_one = 5 != 7  # return True
+
+# bool type vs non bool type
+
+my_baby_bool = "true"
+
+print(type(my_baby_bool)) # <class 'str'>
+
+my_baby_bool_two = True
+
+print(type(my_baby_bool_two)) # <class 'bool'>
+
+
+
+
+ 
