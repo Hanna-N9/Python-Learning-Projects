7eb97742d950c6a4a534f75821c3224d217499de
Hanna Negash <hnegash.n@gmail.com>
2024-03-09 21:00:06 -0800

    Add details

--- lessons/lesson_2.py
+++ lessons/lesson_2.py
@@ -128,5 +128,99 @@ if not (credits >= 120) and not (gpa >= 2.0):
                                   #_______________________________________________________________
 
 # Else Statements
+# allow us to describe what we want our code to do when certain conditions are not met.
+
+if age >= 13:
+  print("Access granted.")
+else:
+  print("Sorry, you must be 13 or older to watch this movie.")
+  
+credits = 120
+gpa = 1.9
+
+if (credits >= 120) and (gpa >= 2.0):
+  print("You meet the requirements to graduate!")
+else:
+  print("You do not meet the requirements to graduate.")
 
 
+                                  #_______________________________________________________________
+
+# Else If Statements (elif)
+# An elif statement checks another condition after the previous if statements conditions aren’t met. --  additional checks into if statements
+  # We can use elif statements to control the order we want our program to check each of our conditional statements. 
+    # First, the if statement is checked, then each elif statement is checked from top to bottom
+    # then finally the else code is executed if none of the previous conditions have been met.
+    
+# If $600.00, the code first checks if that is over 1000, which it is not, then it checks if it’s over 500, which it is, so it prints that message, then because all of the other statements are elif and else, none of them get checked and no more messages get printed.
+
+donation = 600
+
+print("Thank you for the donation!")
+
+if donation >= 1000:
+  print("You've achieved platinum status")
+elif donation >= 500:
+  print("You've achieved gold donor status")
+elif donation >= 100:
+  print("You've achieved silver donor status")
+else:
+  print("You've achieved bronze donor status")
+# return Thank you for the donation! You've achieved gold donor status
+
+
+# letter grade
+
+grade = 86
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")
+else:
+  print("F")
+# return B
+
+
+                                  #_______________________________________________________________
+
+# Overall of Control Flow
+
+# Little Codey is an interplanetary space boxer who is trying to win championship belts for various weight categories on other planets within the solar system.
+  # Checking which number planet is equal to.
+  # Computing Codey’s weight on the destination planet.
+  
+  # #	Planet	  Relative Gravity
+  # 1	Venus	     0.91
+  # 2	Mars	     0.38
+  # 3	Jupiter	   2.34
+  # 4	Saturn	   1.06
+  # 5	Uranus	   0.92
+  # 6	Neptune	   1.19
+  
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 185
+planet = 3
+
+if planet == 1:
+  weight = weight * 0.91
+elif planet == 2:
+  weight = weight * 0.38
+elif planet == 3:
+  weight = weight * 2.34
+elif planet == 4:
+  weight = weight * 1.06
+elif planet == 5:
+  weight = weight * 0.92
+elif planet == 6:
+  weight = weight * 1.19
+
+print("Your weight:", weight) # Your weight: 432.9
