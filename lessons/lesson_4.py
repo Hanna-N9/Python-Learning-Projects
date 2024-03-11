2662d8301a3a5ad12fd677eea3ff5b4d9dec0daf
Hanna Negash <hnegash.n@gmail.com>
2024-03-11 13:27:33 -0700

    Add a brief codes of user input

---
+++ lessons/lesson_4.py
@@ -0,0 +1,8 @@
+# How to assign variables with user input?
+  # While we output a variable’s value using print(), we assign information to a variable using input(). 
+  # The input() function requires a prompt message, which it will print out for the user before they enter the new information.
+
+# 1. The program would print "What is your favorite color? " for the user.
+# 2. The user would enter an answer (e.g., "My favorite color is green!") and press enter.
+# 3. The variable favorite_color would be assigned a value of the user’s answer.
+favorite_color = input("What is your favorite color? ")
