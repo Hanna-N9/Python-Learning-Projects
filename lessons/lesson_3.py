e1fb9b8f9a5cdbc4f264b36075f9202053ec0187
Hanna Negash <hnegash.n@gmail.com>
2024-03-11 13:13:54 -0700

    Add details in comment

---
+++ lessons/lesson_3.py
@@ -0,0 +1,31 @@
+# Python refers to the mistakes within the program as errors and will point to the location where an error occurred with a ^ character. 
+# When programs throw errors that we didn’t expect to encounter, we call those errors bugs. 
+# Programmers call the process of updating the program so that it no longer produces bugs debugging.
+# The interpreter will tell us where (the file name and line number) it got into trouble and its best guess as to what is wrong.
+
+# Common errors:
+  # SyntaxError: Error caused by not following the proper structure (syntax) of the language.
+  # NameError: Errors reported when the interpreter detects a variable that is unknown.
+  # TypeError: Errors thrown when an operation is applied to an object of an inappropriate type.
+  
+                                    #_______________________________________________________________
+                                    
+# SyntaxError
+  # Some common syntax errors are:
+    # Misspelling a Python keyword.
+    # Missing colon :.
+    # Missing closing parenthesis ), square bracket ], or curly brace }.
+
+                                    #_______________________________________________________________
+                                    
+# NameError
+  # Some common name errors are:
+    # Misspelling a variable name.
+    # Forgetting to define a variable.
+    
+                                    #_______________________________________________________________
+                                    
+# TypeError
+  # Some common type errors are:
+    # Accidentally adding or subtracting a string value.
+    # Call a function on something of the incorrect type.
