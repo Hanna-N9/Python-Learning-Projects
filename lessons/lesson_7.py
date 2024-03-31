ea10545ab831dae96fa38e270e13066d31ebcba6
Hanna Negash <hnegash.n@gmail.com>
2024-03-30 22:57:49 -0700

    Add codes on defining a function

---
+++ lessons/lesson_7.py
@@ -0,0 +1,18 @@
+# Functions allow tasks to be performed multiple times within a program without being rewritten. For reusability purpose
+
+# Defining a Function
+
+# A function definition --- Function's core in Python
+  # The def keyword indicates the beginning of a function (also known as a function header). After the function header is 
+  # named in snake_case format that describes the task the function performs. 
+    # def function_name():
+
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station")
+  print("Take the Northbound N, Q, R, or W train 1 stop")
+  print("Get off the Times Square 42nd Street stop")
+  
+  
+                                      #_______________________________________________________________
+
+ 
