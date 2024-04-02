b0161348408a5e8a80f5dd06a81a79049acc37ce
Hanna Negash <hnegash.n@gmail.com>
2024-04-01 19:58:49 -0700

    Add codes on parameters and arguments

--- lessons/lesson_7.py
+++ lessons/lesson_7.py
@@ -41,3 +41,9 @@ weather_check()
                                       #_______________________________________________________________
 
 # Parameters & Arguments
+
+def generate_trip_instructions(location):
+  print(f"Looks like you are planning a trip to visit {location}")
+  print(f"You can use the public subway system to get to {location}")
+
+generate_trip_instructions("Grand Central Station")
