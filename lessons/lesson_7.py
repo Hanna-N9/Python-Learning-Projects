173df32a4e39099fa65bb83eba6746b532467e63
Hanna Negash <hnegash.n@gmail.com>
2024-04-10 17:55:28 -0700

    Add overall codes on functions

--- lessons/lesson_7.py
+++ lessons/lesson_7.py
@@ -158,3 +158,33 @@ print(most_popular1) # Rome
 print(most_popular2) # Venice
 print(most_popular3) # Florence
 
+
+                                      #_______________________________________________________________
+
+# Overall of Functions
+
+# Create a function called trip_planner_welcome() that takes one parameter called name
+def trip_planner_welcome(name):
+  print(f"Welcome to tripplanner v1.0 {name}")
+
+trip_planner_welcome("Jack")
+
+# Define a function called estimated_time_rounded() that will allow us to calculate a rounded time value based on a decimal for
+# our user’s trip
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(2.5)
+
+# Create a function called destination_setup() that will have four parameters
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+  print(f"Your trip starts off in {origin}")
+  print(f"And you are traveling to {destination}")
+  print(f"You will be traveling by {mode_of_transport}")
+  print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("Bali", "Singapore", estimate, "Airplane")
+
+
+
