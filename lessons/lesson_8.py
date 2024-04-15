0cb408a523ed5dd4eb0249adde5e0d7f22d7c857
Hanna Negash <hnegash.n@gmail.com>
2024-04-14 20:49:44 -0700

    Add codes on intro to python dictionaries

---
+++ lessons/lesson_8.py
@@ -0,0 +1,13 @@
+# A dictionary is an unordered set of key: value pairs. It provides us with a way to map pieces of data to each other 
+# so that we can quickly find values that are associated with one another.
+
+# Introduction to Python Dictionaries
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+
+num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
+
+print(sensors)
+
+
+                                      #_______________________________________________________________
