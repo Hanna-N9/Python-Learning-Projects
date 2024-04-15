300b05fa5218363835ab562c5624ee0c56bac5ed
Hanna Negash <hnegash.n@gmail.com>
2024-04-14 21:04:18 -0700

    Add codes on invalid keys

--- lessons/lesson_8.py
+++ lessons/lesson_8.py
@@ -16,3 +16,19 @@ print(sensors)
 
 # Create a dictionary called translations that maps the following words in English to the words in Spanish
 translations = {"mountain": "montaña", "bread": "pan", "friend": "amigo", "horse": "caballo"}
+
+
+                                      #_______________________________________________________________
+
+# Invalid Keys
+   # We can have a list or a dictionary as a value of an item in a dictionary, but we cannot use these data types as keys of the 
+   # dictionary. If we try to, we will get a TypeError.
+   # Dictionaries in Python rely on each key having a hash value, a specific identifier for the key. If the key can change, that hash 
+   # value would not be reliable. So the keys must always be unchangeable, hashable data types, like numbers or strings.
+   
+# Gets an error 'TypeError: unhashable type' --- “unhashable” in this context means that this ‘list’ is an object that can be changed
+  # children = {["Johannes", "Rosmarie", "Eleonore"]: "von Trapp", ["Sonny", "Fredo", "Michael"]: "Corleone"}
+
+# Without error
+children = {'von Trapp': ['Johannes', 'Rosmarie', 'Eleonore'], 'Corleone': ['Sonny', 'Fredo', 'Michael']}
+
