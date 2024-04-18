d3aa42dc9c2e151e1a39b18039569d844cad4563
Hanna Negash <hnegash.n@gmail.com>
2024-04-17 20:34:46 -0700

    Add codes on getting a key to access its values

---
+++ lessons/lesson_8/lesson_8_sub_2.py
@@ -0,0 +1,12 @@
+# Get A Key
+  # Once you have a dictionary, you can access the values in it by providing the key.
+  
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", 
+                   "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+print(zodiac_elements["earth"]) # ['Taurus', 'Virgo', 'Capricorn']
+print(zodiac_elements["fire"]) # ['Aries', 'Leo', 'Sagittarius']
+
+
+
+
