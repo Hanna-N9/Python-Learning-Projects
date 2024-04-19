c01b66d5ef84898941f529a5317e220d0acf96cc
Hanna Negash <hnegash.n@gmail.com>
2024-04-18 20:25:44 -0700

    Add codes on the preferred get key

--- lessons/lesson_8/lesson_8_sub_2.py
+++ lessons/lesson_8/lesson_8_sub_2.py
@@ -15,6 +15,22 @@ print(zodiac_elements["fire"]) # ['Aries', 'Leo', 'Sagittarius']
 # Use an if statement to check if "energy" is a key in zodiac_elements. This will return false instead of throwing an error ('KeyError')
 if "energy" in zodiac_elements:
   print(zodiac_elements["energy"])
+  
+  
+                                        #_______________________________________________________________
 
+# Safely Get a Key
+  # Dictionaries have a .get() method to search for a value instead of the my_dict[key] notation we have been using. If the key you 
+  # are trying to .get() does not exist, it will return None by default
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+# Use .get() to get the value of "teraCoder"‘s user ID, with 100000 as a default value if the user doesn’t exist
+tc_id = user_ids.get("teraCoder", 100000)
+print(tc_id) # 100019
+
+# Getting a key that doesn’t exist! 
+stack_id = user_ids.get("superStackSmash", 100000)
+print(stack_id) # 100000
 
 
