879b0060c9923eacafbfbb86820809db7a4a2fcd
Hanna Negash <hnegash.n@gmail.com>
2024-04-18 20:39:53 -0700

    Add codes on deleting a key

--- lessons/lesson_8/lesson_8_sub_2.py
+++ lessons/lesson_8/lesson_8_sub_2.py
@@ -34,3 +34,31 @@ stack_id = user_ids.get("superStackSmash", 100000)
 print(stack_id) # 100000
 
 
+                                        #_______________________________________________________________
+
+# Delete a Key
+  # Use .pop() to delete a key and we can provide a default value to return if the key does not exist in the dictionary.
+  
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, 
+                   "power stew": 30}
+
+health_points = 20
+
+# Add the corresponding value of the key "stamina grains" to the health_points variable and remove it from the dictionary. 
+# If the key does not exist, add 0 to health_points.
+health_points += available_items.pop("stamina grains", 0)
+
+# Add the value of "power stew" to health_points and remove the item from the dictionary. If the key does not exist, add 0 to health_points.
+health_points += available_items.pop("power stew", 0)
+
+# Add the value of "mystic bread" to health_points and remove the item from the dictionary. If the key does not exist, add 0 to 
+# health_points
+health_points += available_items.pop("mystic bread", 0)
+
+print(available_items) # {'health potion': 10, 'cake of the cure': 5, 'green elixir': 20, 'strength sandwich': 25}
+print(health_points)  # 65
+
+
+
+
+
