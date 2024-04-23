012f4071051374b459fb53cea3ab560095a0c56b
Hanna Negash <hnegash.n@gmail.com>
2024-04-22 19:24:58 -0700

    Get all keys in dictionary

--- lessons/lesson_8/lesson_8_sub_2.py
+++ lessons/lesson_8/lesson_8_sub_2.py
@@ -59,6 +59,26 @@ print(available_items) # {'health potion': 10, 'cake of the cure': 5, 'green eli
 print(health_points)  # 65
 
 
+                                        #_______________________________________________________________
+
+# Get All Keys
+  # list() function to get all keys
+  # A .keys() method that returns a dict_keys object. A dict_keys object is a view object, which provides a look at the current 
+  # state of the dictionary, without the user being able to modify anything.
+  
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+# Create a variable called users and assign it to be a dict_keys object of all of the keys of the user_ids dictionary.
+users = user_ids.keys()
+
+# Create a variable called lessons and assign it to be a dict_keys object of all of the keys of the num_exercises dictionary
+lessons = num_exercises.keys()
+
+print(users)
+print(lessons)
+
+
 
 
 
