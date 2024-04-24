39321239dc3b2e9a7a66486998b307e33d0d699f
Hanna Negash <hnegash.n@gmail.com>
2024-04-23 22:18:08 -0700

    Get all keys and values in dictionary

--- lessons/lesson_8/lesson_8_sub_2.py
+++ lessons/lesson_8/lesson_8_sub_2.py
@@ -103,3 +103,18 @@ for exercises in num_exercises.values():
 print(total_exercises)
 
 
+                                        #_______________________________________________________________
+
+# Get All Items
+  # Get both the keys and the values with the .items() method. Like .keys() and .values(), it returns a dict_list object. 
+  # Each element of the dict_list returned by .items() is a tuple consisting of: (key, value)
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for occupation, percentage in pct_women_in_occupation.items():
+  print(f"Women make up {percentage} percent of {occupation}s.")
+
+
+
+
+
