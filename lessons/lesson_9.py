849cccf72c41b7b5809b6b89a8692daa8b5778b9
Hanna Negash <hnegash.n@gmail.com>
2024-05-07 14:22:54 -0700

    Add codes about Python types

---
+++ lessons/lesson_9.py
@@ -0,0 +1,18 @@
+# Types
+  # Python equips us with many different ways to store data. Like int, float, string, list, dict and so on...
+  # Use the type() function to check the type of a Python data.
+
+# Call type() on the integer 5 and print the results.
+print(type(5)) # <class 'int'>
+
+
+# Define a dictionary my_dict using curly braces {}.
+my_dict = {}
+print(type(my_dict)) # <class 'dict'>
+
+
+# Define a list called my_list
+my_list = []
+print(type(my_list)) # <class 'list'>
+
+
