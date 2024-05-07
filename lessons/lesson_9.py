35ac759cda82af207966bd7570569db80a2c7c1c
Hanna Negash <hnegash.n@gmail.com>
2024-05-07 14:29:23 -0700

    Add codes on a basic class

--- lessons/lesson_9.py
+++ lessons/lesson_9.py
@@ -16,3 +16,17 @@ my_list = []
 print(type(my_list)) # <class 'list'>
 
 
+                                    #_______________________________________________________________
+
+# Class
+  # A class is a template for a data type. It describes the kinds of information that class will hold and how a programmer will 
+  # interact with that data. 
+  # Define a class using the class keyword. 
+  # PEP 8 Style Guide for Python Code recommends capitalizing the names of classes to make them easier to identify.
+
+# Define an empty class called Facade. 
+class Facade:
+    pass
+
+
+
