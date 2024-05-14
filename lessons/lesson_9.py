631687745278ec3c315b20b87ecb938f0b3329de
Hanna Negash <hnegash.n@gmail.com>
2024-05-14 15:16:42 -0700

    Add codes on class variables

--- lessons/lesson_9.py
+++ lessons/lesson_9.py
@@ -64,3 +64,23 @@ print(type(cool_instance)) # "<class '__main__.CoolClass'>"
 
 facade_1_type = type(facade_1)
 print(facade_1_type) # <class '__main__.Facade'>
+
+
+                                    #_______________________________________________________________
+
+# Class Variables
+  # A class variable is a variable that’s the same for every instance of the class. You can define a class variable by including it in 
+  # the indented part of your class definition, and you can access all of an object’s class variables with object.variable syntax.
+  # Class variables are often referenced with a leading period, like .title above. This is done to quickly show that the variable belongs
+  # to a class and must be accessed with dot notation, like drummer.title.
+
+class Musician:
+  title = "Rockstar" # class variable
+
+drummer = Musician() # instantiated
+print(drummer.title) # prints "Rockstar"
+
+
+# Create a Grade class with a class attribute .minimum_passing equal to 65.
+class Grade:
+  minimum_passing = 65
