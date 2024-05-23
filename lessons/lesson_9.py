26450db1500630c7cc852ee6192b2a1ce7e33966
Hanna Negash <hnegash.n@gmail.com>
2024-05-22 17:48:44 -0700

    Add codes about Attribute Functions

--- lessons/lesson_9.py
+++ lessons/lesson_9.py
@@ -214,6 +214,60 @@ print(dog_2.name)  # Output: Luna
 print(dog_2.age)   # Output: 7
 
 
+                                    #_______________________________________________________________
+# Attribute Functions
+  # Instance variables and class variables are both accessed similarly in Python. They are both considered attributes of an object. 
+  # If we attempt to access an attribute that is neither a class variable nor an instance variable of the object Python will throw an 
+  # AttributeError. 
+  
+  # What if we aren’t sure if an object has an attribute or not? hasattr() will return True if an object has a given attribute and 
+  # False otherwise. If we want to get the actual value of the attribute, getattr() is a Python function that will return the value of a 
+  # given object and attribute. In this function, we can also supply a third argument that will be the default if the object does not have 
+  # the given attribute.
+
+# The syntax and parameters for these functions look like this:
+
+# hasattr(object, “attribute”) has two parameters:
+  # object : the object we are testing to see if it has a certain attribute
+  # attribute : name of attribute we want to see if it exists
+
+# getattr(object, “attribute”, default) has three parameters (one of which is optional): 
+  # object : the object whose attribute we want to evaluate
+  # attribute : name of attribute we want to evaluate
+  # default : the value that is returned if the attribute does not exist (note: this parameter is optional)
+
+class NoCustomAttributes:
+  pass
+
+attributeless = NoCustomAttributes()
+
+try:
+  attributeless.fake_attribute
+except AttributeError:
+  print("This text gets printed!") # prints "This text gets printed!"
+
+# if the attributeless object has the attribute .fake_attribute. Since it does not, hasattr() returned False. 
+hasattr(attributeless, "fake_attribute") # returns False
+
+# Since .other_fake_attribute isn’t a real attribute on attributeless, our call to getattr() returned the supplied default value 800
+getattr(attributeless, "other_fake_attribute", 800) # returns 800, the default value
+
+getattr(dog_1, "name", 800) # returns Buddy
+
+
+#  a list of different data types: a dictionary, a string, an integer, and a list
+can_we_count_it = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
+
+# For every element in can_we_count_it, check if the element has the attribute .count using the hasattr() function, else
+for element in can_we_count_it:
+  if hasattr(element, "count"):
+    print(str(type(element)) + " has the count attribute!")
+  else:
+    print(str(type(element)) + " does not have the count attribute :(")
+
+
+
+
 
 
 
